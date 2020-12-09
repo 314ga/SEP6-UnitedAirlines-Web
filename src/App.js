855f9c29e18349cdf8308c5bd050be7b8056fbdd
314ga <jakub.piga.cp@gmail.com>
2020-12-09 11:23:27 +0100

    Added local web-page code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+
+import './App.css';
+import FlightsPage from './components/FlightsPage';
+import ManufacturerPage from './components/ManufacturerPage';
+import WeatherPage from './components/WeatherPage';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const value = 'kubi';
-  return <div>Hello {value}</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/WeatherPage" component={WeatherPage} />
+        <Route exact path="/" component={FlightsPage} />
+        <Route exact path="/ManufacturerPage" component={ManufacturerPage} />
+
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import store from './store'
+import { Provider } from 'react-redux'
+import { retrieveFlightData } from './reducers/flightData'
+import { retrieveWeatherData } from './reducers/weatherData'
+import { retrieveManufacturerData } from './reducers/manufacturerData'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * Getting default data from the server 
+ */
+/*TODO: Initial load
+store.dispatch(retrieveFlightData("flights-per-month"));
+store.dispatch(retrieveWeatherData("weather/init"));
+store.dispatch(retrieveManufacturerData("flight/init-man"));*/
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
